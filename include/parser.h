b0dca5d884a15a2cadfab5a2c8a74dda3365d602
Sean Dahan <aui.svi@gmail.com>
2021-10-12 22:16:21 +0300

    Added parser.h

---
+++ include/parser.h
@@ -0,0 +1,12 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "token.h"
+
+// Might change return type to custom type refering if the parsing was succseful or not
+void begin(Token* tk);
+
+
+#endif // !PARSER_H
