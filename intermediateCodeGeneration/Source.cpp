6597a20d63f91dd0d9320f983d4de8a4e1d9a3c6
Yahel Kevelson <yahelkev@gmail.com>
2021-10-14 21:01:00 +0300

    able to convert given int init line to asm x86 mavc

--- intermediateCodeGeneration/Source.cpp
+++ intermediateCodeGeneration/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define INT_VAR_SIZE 4
+
 void initIntAsm(std::string name, int value);
 
 int main()
@@ -10,5 +12,6 @@ int main()
 
 void initIntAsm(std::string name, int value)
 {
-	std::cout<< "*asm code*" <<std::endl;
+	std::cout<< "sub esp," << INT_VAR_SIZE <<std::endl;
+	std::cout<< "mov DWORD PTR _" + name + "$[ebp]," << value <<std::endl;
 }
