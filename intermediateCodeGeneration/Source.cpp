c720be35299da013c4532838ed084659c393a205
Yahel Kevelson <yahelkev@gmail.com>
2021-10-14 21:26:01 +0300

    Done (I think)

--- intermediateCodeGeneration/Source.cpp
+++ intermediateCodeGeneration/Source.cpp
@@ -2,15 +2,19 @@
 
 #define INT_VAR_SIZE 4
 
-void initIntAsm(std::string name, int value);
+void initIntAsm(std::string name, std::string value);
 
 int main()
 {
-	initIntAsm("var", 23);
+	std::string AST[4] = { "var", "int", "=", "29" };
+	if (AST[1] == "int")
+	{
+		initIntAsm(AST[0], AST[3]);
+	}
 	return 0;
 }
 
-void initIntAsm(std::string name, int value)
+void initIntAsm(std::string name, std::string value)
 {
 	std::cout<< "sub esp," << INT_VAR_SIZE <<std::endl;
 	std::cout<< "mov DWORD PTR _" + name + "$[ebp]," << value <<std::endl;
