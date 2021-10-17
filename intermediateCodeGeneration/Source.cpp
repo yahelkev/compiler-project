bdcdd35cb6b6e8e84af53dd8449aeec5890c2554
Yahel Kevelson <yahelkev@gmail.com>
2021-10-17 19:23:10 +0300

    writing to file instead of printing to console

--- intermediateCodeGeneration/Source.cpp
+++ intermediateCodeGeneration/Source.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <fstream>
 
 #define INT_VAR_SIZE 4
+#define ASM_FILE "../out.asm"
 
 void initIntAsm(std::string name, std::string value);
 
 int main()
 {
-	std::string AST[4] = { "var", "int", "=", "29" };
+	std::string AST[4] = { "var", "int", "=", "30" };
 	if (AST[1] == "int")
 	{
 		initIntAsm(AST[0], AST[3]);
@@ -16,5 +18,7 @@ int main()
 
 void initIntAsm(std::string name, std::string value)
 {
-	std::cout<< "mov     dword ptr[rbp - " << INT_VAR_SIZE << "], " << value <<std::endl;
+	std::ofstream outfile;
+	outfile.open(ASM_FILE, std::ios_base::app);
+	outfile << "mov     dword ptr[rbp - " << INT_VAR_SIZE << "], " << value <<std::endl;
 }
