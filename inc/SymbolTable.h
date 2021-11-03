77d7325e28f7f1b832bccdf2b6cada94ccc72712
Sean Dahan <aui.svi@gmail.com>
2021-11-03 17:52:11 +0200

    Added symbol table basis

---
+++ inc/SymbolTable.h
@@ -0,0 +1,48 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+#define bool int
+#define false 0
+#define true 1
+
+
+// C Style
+typedef struct TABLE_VALUE {
+    enum {
+        FUNCTION_TAG, VARIABLE_TAG
+    }tag;
+    int line;
+    int column;
+
+    union {
+        
+        struct varibale {
+            char* type;
+        }* varialble_;
+
+        struct function {
+            struct variable* args;
+            char* returntype;
+        }* function_;
+    };
+}TABLE_VALUE;
+
+
+typedef struct Table {
+    int size;
+    char** keys;
+    TABLE_VALUE* values;
+}Table;
+
+void newTable(Table* table);
+bool isDefined(Table* table, char* key);
+TABLE_VALUE getValue(Table* table, char* key);
+/* Return :
+    1 - Insertion succesded
+    0 - Insertion Failed
+*/
+bool insertValue(Table* table, char* key, TABLE_VALUE value);
+
+
+
+#endif // !SYMBOL_TABLE_H
