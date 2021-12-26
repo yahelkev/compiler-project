ed05e52c20545d2c6078b67552920d3a326ceec9
Sean Dahan <aui.svi@gmail.com>
2021-12-26 17:15:23 +0200

    Finished base file creation

--- compiler/CodeGen.c
+++ compiler/CodeGen.c
@@ -5,3 +5,32 @@ void newCodeGen(CodeGen* gen, char* path) {
 	strncpy(gen->filePath, path, LENGTH(path));
 	return;
 }
+
+void freeCodeGen(CodeGen* gen) {
+	free(gen->filePath);
+	fclose(gen->filePointer);
+	free(gen);
+	return;
+}
+
+FILE* CreateBlankFile(const char* path) {
+	FILE* fp;
+	char* asmPath = (char*)malloc(sizeof(char) * LENGTH(path));
+	strncpy(asmPath, path, LENGTH(path));
+	int pos = 0;
+	for (size_t i = 0; i < strlen(asmPath); i++)
+		if (asmPath[i] == '.') pos = i;
+	asmPath[pos] = '\0';
+	asmPath = (char*)realloc(asmPath, sizeof(char) * LENGTH(asmPath) + LENGTH(ASM_EXTENSION));
+	strncat(asmPath, ASM_EXTENSION, LENGTH(ASM_EXTENSION));
+
+	fp = fopen(asmPath, "w");
+	char* btext = (char*)malloc(sizeof(char) * LENGTH(START_OF_FILE));
+	strncpy(btext, START_OF_FILE, LENGTH(START_OF_FILE));
+	btext = (char*)realloc(btext, sizeof(char) * LENGTH(btext) + strlen(path));
+	strncat(btext, path, LENGTH(path));
+	fwrite(btext, sizeof(char), strlen(btext), fp);
+	return fp;
+}
+
+
--- compiler/CodeGen.h
+++ compiler/CodeGen.h
@@ -3,12 +3,18 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <io.h>
+#include <stdio.h>
 
 #define LENGTH(var) strlen(var) + 1
+#define START_OF_FILE "\t.file "
+#define ASM_EXTENSION ".asm"
 
 typedef struct CodeGen {
 	char* filePath;
+	FILE* filePointer;
 } CodeGen;
 
 void newCodeGen(CodeGen* gen, char* path);
+FILE* CreateBlankFile(char* path);
 #endif // !CODE_GEN_H
