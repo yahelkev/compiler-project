49fe16595c32d18172c2fdafab02595170c331da
Yahel Kevelson <yahelkev@gmail.com>
2022-02-02 23:16:35 +0200

    Added basic structs

---
+++ compiler/Assembler.h
@@ -0,0 +1,33 @@
+#pragma once
+
+typedef struct fileHeader {
+	unsigned short pcType;
+	unsigned short numSections;
+	unsigned int timedate;
+	unsigned int symbolTablePtr;
+	unsigned int numOfSymbols;
+	unsigned short OpHeaderSize;
+	unsigned short flags;
+}fileHeader;
+
+typedef struct sectionHeadrer {
+	unsigned char name[8];
+	unsigned int virtualSize;
+	unsigned int virtualAddress;
+	unsigned int rawDataSize;
+	unsigned int dataAddress;
+	unsigned int ptrToRelocation;
+	unsigned int lineNumbrsPtr;
+	unsigned short numOfRelocations;
+	unsigned short numOfLineNums;
+	unsigned int flags;
+}sectionHeadrer;
+
+typedef struct symbolsTable {
+	unsigned char name[8];
+	unsigned int value;
+	unsigned short sectionNum;
+	unsigned short type;
+	unsigned char storageClass;
+	unsigned char numOfAuxSymbols;
+}sectionHeadrer;
