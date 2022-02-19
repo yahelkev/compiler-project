d10a85baf564c8635fc7668f421194046b9f7e18
Yahel Kevelson <yahelkev@gmail.com>
2022-02-19 23:45:42 +0200

    Functions added to .o file

--- compiler/Assembler.c
+++ compiler/Assembler.c
@@ -20,7 +20,14 @@ void runAssembler(Assembler* asm) {
     setSectionHeaders(asm->_obj->_dataHeaders, 0, 0, 0, 0, 0, 0, 0, 0, DATA_CHARACTERISTICS);
     setSectionHeaders(asm->_obj->_bssHeaders, 0, 0, 0, 0, 0, 0, 0, 0, BSS_CHARACTERISTICS);
     addSymbol(asm->_obj, ".file", 0, MAGE_SYM_DEBUG, 0, IMAGE_SYM_CLASS_FILE, 0);
-   //TO DO: functions
+
+    for (size_t i = 0; i < asm->table->size; i++) {
+        if (FUNCTION_TAG == asm->table->values[i]->tag) {
+            printf("%s\n", asm->table->keys[i]);
+            addSymbol(asm->_obj, asm->table->keys[i], 0, TEXT_SECTION_NUM, DT_FUNCTION, IMAGE_SYM_CLASS_EXTERNAL, 0);
+        }
+    }
+
     addSymbol(asm->_obj, ".text", 0, TEXT_SECTION_NUM, 0, IMAGE_SYM_CLASS_STATIC, 0);
     addSymbol(asm->_obj, ".data", 0, DATA_SECTION_NUM, 0, IMAGE_SYM_CLASS_STATIC, 0);
     addSymbol(asm->_obj, ".bss", 0, BSS_SECTION_NUM, 0, IMAGE_SYM_CLASS_STATIC, 0);
